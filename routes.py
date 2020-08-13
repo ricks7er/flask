1ca19413bdf1c941c83dcd71368793ea386382bc
Jose <jrortegar@gmail.com>
2020-08-13 00:42:11 -0500

    eighth commit

--- routes.py
+++ routes.py
@@ -21,3 +21,19 @@ def add():
         flash('Tarea adicionada a la Base de Datos')
         return redirect(url_for('index'))
     return render_template('add.html',form=form)
+
+@app.route('/edit/<int:task_id>',methods=['GET','POST'])
+def edit(task_id):
+    task = Task.query.get(task_id)
+    form = forms.AddTaskForm()
+
+    if task:
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = datetime.utcnow()
+            db.session.commit()
+            flash('Tarea actualizada')
+            return redirect(url_for('index'))
+        form.title.data=task.title
+        return render_template('edit.html',form=form,task_id=task_id)
+    return redirect(url_for('index'))
